cfcfe7f2111f6315dde1724784ff2db2ed4a112c
yasklerev <yasklerev.lannet@gmail.com>
2021-03-30 21:13:30 -0300

    Primer avance proyecto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/auth/components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'iniciarSesion', component: LoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'iniciarSesion' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/hostel/hostel.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomComponent } from './components/room/room.component';
+
+
+
+@NgModule({
+  declarations: [RoomComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HostelModule { }
